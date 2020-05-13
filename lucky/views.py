6c35c361d54715055ac451cf9c91143c595d5c35
Plutocracy <luckydogbey@gmail.com>
2020-05-13 13:04:02 +0000

    added view functions and templates

--- lucky/views.py
+++ lucky/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+from datetime import datetime
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'lucky_index.html', {'mycontent': posts})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.published_date = datetime.now()
+            post.save()
+            return redirect('/')
+        else:
+            return render(request, 'lucky_post_new.html', {'post_form': form})
+    else:
+        form = PostForm()
+        return render(request, 'lucky_post_new.html', {'post_form': form})
+    
+def post_detail(request, article_id):
+    post = Post.objects.get(article_id)
+    return render(request, 'lucky_post_detail.html', {'myarticle': post})
