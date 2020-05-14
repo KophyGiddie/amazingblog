76e8feb27a2f93ab013425cd3bbbe8965115cf2e
Gideon Boateng <gidboa@gmail.com>
2020-05-14 15:37:34 +0000

    added image support

--- gideon/models.py
+++ gideon/models.py
@@ -6,7 +6,9 @@ class Post(models.Model):
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     category = models.CharField(max_length=200)
     content = models.TextField()
+    is_published = models.BooleanField(default=False)
     published_date = models.DateTimeField(blank=True, null=True)
+    modified_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
--- gideon/views.py
+++ gideon/views.py
@@ -5,7 +5,12 @@ from datetime import datetime
 
 
 def homepage(request):
-    mydata = Post.objects.all()
+    mydata = Post.objects.filter(is_published=True)
+    return render(request, 'gideon_index.html', {'mycontent': mydata})
+
+
+def unpublished_posts(request):
+    mydata = Post.objects.filter(is_published=False)
     return render(request, 'gideon_index.html', {'mycontent': mydata})
 
 
@@ -24,6 +29,54 @@ def post_new(request):
         return render(request, 'gideon_post_new.html', {'post_form': form})
 
 
+def edit_post(request, article_id):
+    my_post = Post.objects.get(id=article_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=my_post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.modified_date = datetime.now()
+            post.save()
+            return redirect('/gideon/')
+        else:
+            return render(request, 'gideon_post_edit.html', {'post_form': form})
+    else:
+        form = PostForm(instance=my_post)
+        return render(request, 'gideon_post_edit.html', {'post_form': form, 'article_id': article_id})
+
+
 def post_detail(request, article_id):
     mydata = Post.objects.get(id=article_id)
     return render(request, 'gideon_post_detail.html', {'myarticle': mydata})
+
+
+def delete_post(request, article_id):
+    mydata = Post.objects.get(id=article_id)
+    mydata.delete()
+    return redirect('/gideon/')
+
+
+def publish_post(request, article_id):
+    if request.method == 'POST':
+        mydata = Post.objects.get(id=article_id)
+        mydata.is_published = True
+        mydata.save()
+
+        return redirect('/gideon/')
+    else:
+        return redirect('/gideon/')
+
+
+def unpublish_post(request, article_id):
+    if request.method == 'POST':
+        mydata = Post.objects.get(id=article_id)
+        mydata.is_published = False
+        mydata.save()
+
+        return redirect('/gideon/')
+    else:
+        return redirect('/gideon/')
+
+
+
+
