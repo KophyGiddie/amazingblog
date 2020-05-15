f7e75855b5f14bad47718f2d47ef79cda3301e09
bright ahedor <brightahedor@gmail.com>
2020-05-15 13:04:55 +0000

    done with assignment

--- bright/forms.py
+++ bright/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+   
     title = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={'class': "form-control"}))
-    content = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': "form-control"})) 
-    
+    content = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': "form-control"}))
+
     class Meta:
         model = Post
         fields = ('title', 'content', 'image',)
--- bright/views.py
+++ bright/views.py
@@ -4,16 +4,20 @@ from .forms import PostForm
 from datetime import datetime
 
 
-#load homepage
+#load homepage and list of post that were published
 def homepage(request):
     mydata = Post.objects.filter(is_published=True)
     return render(request, 'bright_index.html', {'mycontent': mydata})
 
+#lis of post that were not published
+def unpublished_posts(request):
+    mydata = Post.objects.filter(is_published=False)
+    return render(request, 'bright_index.html', {'mycontent': mydata})
 
 #create a new post
 def post_new(request):
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST,request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.published_date = datetime.now()
@@ -24,6 +28,22 @@ def post_new(request):
     else:
         form = PostForm()
         return render(request, 'bright_post_new.html', {'post_form': form})
+
+#edit a post
+def edit_post(request, article_id):
+    my_post = Post.objects.get(id=article_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=my_post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.modified_date = datetime.now()
+            post.save()
+            return redirect('/bright/')
+        else:
+            return render(request, 'bright_post_edit.html', {'post_form': form})
+    else:
+        form = PostForm(instance=my_post)
+        return render(request, 'bright_post_edit.html', {'post_form': form, 'article_id': article_id})
 
 
 #visit page detail page
@@ -43,6 +63,16 @@ def publish_post(request, article_id):
     if request.method == 'POST':
         mydata = Post.objects.get(id=article_id)
         mydata.is_published = True
+        mydata.save()
+        return redirect('/bright/')
+    else:
+        return redirect('/bright/')
+
+#unpublish a particular post
+def unpublish_post(request, article_id):
+    if request.method == 'POST':
+        mydata = Post.objects.get(id=article_id)
+        mydata.is_published = False
         mydata.save()
         return redirect('/bright/')
     else:
