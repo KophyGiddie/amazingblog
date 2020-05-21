e4ab1f9e6cb6327155ef7d88a0130dfb3d3cf84a
Gideon Boateng <gidboa@gmail.com>
2020-05-21 14:20:54 +0000

    debug settings fixed

--- myblog/settings.py
+++ myblog/settings.py
@@ -131,12 +131,9 @@ LOGIN_URL = '/users/login/'
 if 'DATABASE_URL' in os.environ:
     DATABASES['default'] = dj_database_url.config()
 
+    DEBUG = False
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-
-
-
-
 
 
 STATIC_ROOT = 'staticfiles'
