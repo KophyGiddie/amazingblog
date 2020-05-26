08401e79b5c2a9cf9fe464ea9e8faed9bfbe6761
Gideon Boateng <gidboa@gmail.com>
2020-05-26 17:05:08 +0000

    static files for production

--- awesomeblog/settings.py
+++ awesomeblog/settings.py
@@ -6,12 +6,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
-    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -24,7 +23,8 @@ INSTALLED_APPS = [
     'Niicode',
     'annunziata',
     'bright',
-    'users'
+    'users',
+    'storages',
 
 ]
 
@@ -115,6 +115,21 @@ if 'DATABASE_URL' in os.environ:
     DATABASES['default'] = dj_database_url.config()
 
     DEBUG = False
+
+# STATIC FILES SETTINGS IN PRODUCTION
+AWS_STORAGE_BUCKET_NAME = 'blog-static-internship'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
+
+AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
+AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
+
+STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# END STATIC FILES SETTINGS HERE #
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
